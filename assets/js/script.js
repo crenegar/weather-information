920462e2498b0ed121310eb7a2968c37511cbe63
chad renegar <crenegar4@gmail.com>
2021-07-06 14:27:48 -0700

    new java

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+//in0put box
+var enter = document.querySelector('#enter')
+var btn = document.querySelector('#btn')
+
+//today weather
+
+var city = document.querySelector('#city')
+var today = document.querySelector('#today')
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+var humid = document.querySelector('#humid')
+var uvindex = document.querySelector('#uvindex')
+
+// tommorow
+var img1 = document.querySelector('#img1')
+var temperatureday1 = document.querySelector('#temperatureday1')
+var windday1 = document.querySelector('#windday1')
+var humidday1 = document.querySelector('#humidday1'
+)
+
+//tommorow +1
+var img2 = document.querySelector('#img2')
+var temperatureday2 = document.querySelector('#temperatureday2')
+var windday2 = document.querySelector('#windday2')
+var humidday2 = document.querySelector('#humidday2')
+
+// tommorow +2
+var img3 = document.querySelector('#img3')
+var temperatureday3 = document.querySelector('#temperatureday3')
+var windday3 = document.querySelector('#windday3')
+var humidday3 = document.querySelector('#humidday3')
+
+// tommorow +3
+var img4 = document.querySelector('#img4')
+var temeperatureday4 = document.querySelector('#temperatureday4')
+var windday4 = document.querySelector('#windday4')
+var humidday4 = document.querySelector('#humidday4')
+
+// tommorow +4
+var img5 = document.querySelector('#img5')
+var temperatureday5 = document.querySelector('#temperatureday5')
+var windday5 = document.querySelector('#windday5')
+var humidday5 = document.querySelector('#humidday5')
+
+var currentDay = document.querySelector('#currentDay')
+var tomorrowDay = document.querySelector('#tomorrow')
+var day2 = document.querySelector('#day2')
+var day3 = document.querySelector('#day3')
+var day4 = document.querySelector('#day4')
+var day5 = document.querySelector('#day5')
+
+var list = document.createElement('#list')
+var cityBtn = document.createElement('button');
+var cityBtnValue = cityBtn.innerText
+
+
+
